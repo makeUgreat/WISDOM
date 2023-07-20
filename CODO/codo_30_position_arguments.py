34911f5c695576d71cbd8402a6d72a4d935f65a5
김진명 <willom2c@naver.com>
2023-07-20 17:42:31 +0900

    23-07-20

---
+++ CODO/codo_30_position_arguments.py
@@ -0,0 +1,11 @@
+print(10, 20 ,30) # 파라미터 위치 그대로 인자로 받아 출력
+
+def prt_nums(a, b, c):
+    print(a)
+    print(b)
+    print(c)
+prt_nums(10,15,'c') # 위치대로 매개변수 대입 
+
+x = [10, 20, 30]
+
+prt_nums(*x) # 언패킹을 통해 요소 각각 풀어 대입 
