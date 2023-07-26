6e2676cdff41e7220fa12b46bc9317896661f067
MAC_KJM <willom2c@naver.com>
2023-07-26 20:38:40 +0900

    23-07-26

--- MIN_CODING/STEP2/min_11-1.py
+++ MIN_CODING/STEP2/min_11-1.py
@@ -1,28 +1,9 @@
-def input_func():
-    get_num = int(input())
-
-    return get_num 
-
 
 def calc(a,b,c):
-    
     return a + b + c
 
 
+a,b,c = map(int,input().split())
 
 
-
-
-
-
-
-
-
-
-
-def main():
-    a = input_func()
-    b = input_func()
-    c = input_func()
-
-calc(a,b,c)
+print( calc(a,b,c) ) 
