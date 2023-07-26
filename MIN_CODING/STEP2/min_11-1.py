63ad19a327902db9426bc162fd79398e273c2278
김진명 <willom2c@naver.com>
2023-07-26 17:00:50 +0900

    23-07-26

---
+++ MIN_CODING/STEP2/min_11-1.py
@@ -0,0 +1,28 @@
+def input_func():
+    get_num = int(input())
+
+    return get_num 
+
+
+def calc(a,b,c):
+    
+    return a + b + c
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    a = input_func()
+    b = input_func()
+    c = input_func()
+
+calc(a,b,c)
