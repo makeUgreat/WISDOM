3bcadbf7995137704582f448a90c201eec65de6e
HOME_KJM <willom2c@naver.com>
2023-07-31 02:39:39 +0900

    23-07-31

--- MIN_CODING/STEP2/min_15-16.py
+++ MIN_CODING/STEP2/min_15-16.py
@@ -1,19 +1,27 @@
-ch1 = ord(input())-1
+ch1 = ord(input()) - 1
 
-lsts = [] 
+lsts = []
 
-j = 2 
-k = 0 
+j = 2
+k = 0
 i = 4
+
 while i >= 1:
     line = []
     k = i
-    while i <= k+j:
+    while i <= k + j:
         line.append(chr(ch1 + i))
-        i += 1 
+        i += 1
     j -= 1
-    i = int(i / 2)-1 
+    i = int(i / 2) - 1
     lsts.append(line)
 
-for lst in lsts: 
-    print(*lst,sep='')
+lst_index = 0
+while lst_index < len(lsts):
+    lst = lsts[lst_index]
+    element_index = 0
+    while element_index < len(lst):
+        print(lst[element_index], end="")
+        element_index += 1
+    print()
+    lst_index += 1
